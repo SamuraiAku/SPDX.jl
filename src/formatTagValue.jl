4196fce473c62a86dd71f999a1fddf310f8876ce
Simon Avery <savery@ieee.org>
2021-12-30 18:13:52 -0800

    Use abstract type IO instead of concrete type IOBuffer in method definitions.

--- src/formatTagValue.jl
+++ src/formatTagValue.jl
@@ -1,9 +1,9 @@
 
 
-convert_to_TagValue!(TagValueDoc::IOBuffer, dataElement::Union{AbstractSpdx, AbstractSpdxElement})= write(TagValueDoc, string(dataElement) * "\n")  # Default
-convert_to_TagValue!(TagValueDoc::IOBuffer, stringElement::AbstractString)= write(TagValueDoc, stringElement * "\n")
+convert_to_TagValue!(TagValueDoc::IO, dataElement::Union{AbstractSpdx, AbstractSpdxElement})= write(TagValueDoc, string(dataElement) * "\n")  # Default
+convert_to_TagValue!(TagValueDoc::IO, stringElement::AbstractString)= write(TagValueDoc, stringElement * "\n")
 
-function write_TagValue!(TagValueDoc::IOBuffer, element, TagValueName::Union{Nothing, String}, Multiline::Bool)
+function write_TagValue!(TagValueDoc::IO, element, TagValueName::Union{Nothing, String}, Multiline::Bool)
     if TagValueName !== nothing
         write(TagValueDoc, string(TagValueName) * ":  ")
         if Multiline == true
@@ -16,7 +16,7 @@ function write_TagValue!(TagValueDoc::IOBuffer, element, TagValueName::Union{Not
     end
 end
 
-function convert_to_TagValue!(TagValueDoc::IOBuffer, doc::AbstractSpdxData, NameTable::Table)
+function convert_to_TagValue!(TagValueDoc::IO, doc::AbstractSpdxData, NameTable::Table)
     for idx in range(1,length= length(NameTable))
         fieldval= getproperty(doc, NameTable.Symbol[idx])
         (ismissing(fieldval) || (isa(fieldval, Vector) && isempty(fieldval))) && continue  # goto next symbol if this one has no data
@@ -34,17 +34,17 @@ function convert_to_TagValue!(TagValueDoc::IOBuffer, doc::AbstractSpdxData, Name
     return nothing
 end
 
-convert_to_TagValue!(TagValueDoc::IOBuffer, doc::SpdxDocumentV2)= convert_to_TagValue!(TagValueDoc, doc, SpdxDocumentV2_NameTable)
-convert_to_TagValue!(TagValueDoc::IOBuffer, pkg::SpdxPackageV2) = convert_to_TagValue!(TagValueDoc, pkg, SpdxPackageV2_NameTable)
-convert_to_TagValue!(TagValueDoc::IOBuffer, info::SpdxCreationInfoV2)= convert_to_TagValue!(TagValueDoc, info, SpdxCreationInfoV2_NameTable)
+convert_to_TagValue!(TagValueDoc::IO, doc::SpdxDocumentV2)= convert_to_TagValue!(TagValueDoc, doc, SpdxDocumentV2_NameTable)
+convert_to_TagValue!(TagValueDoc::IO, pkg::SpdxPackageV2) = convert_to_TagValue!(TagValueDoc, pkg, SpdxPackageV2_NameTable)
+convert_to_TagValue!(TagValueDoc::IO, info::SpdxCreationInfoV2)= convert_to_TagValue!(TagValueDoc, info, SpdxCreationInfoV2_NameTable)
 
 
-function convert_to_TagValue!(TagValueDoc::IOBuffer, PkgRef::SpdxPackageExternalReferenceV2)
+function convert_to_TagValue!(TagValueDoc::IO, PkgRef::SpdxPackageExternalReferenceV2)
     write(TagValueDoc, PkgRef.Category * " " * PkgRef.RefType * " " * PkgRef.Locator * "\n")
     ismissing(PkgRef.Comment) || write_TagValue!(TagValueDoc, PkgRef.Comment, SpdxPackageExternalReferenceV2_NameTable.TagValueName[4], SpdxPackageExternalReferenceV2_NameTable.Multiline[4])
 end
 
-function convert_to_TagValue!(TagValueDoc::IOBuffer, relationship::SpdxRelationshipV2)
+function convert_to_TagValue!(TagValueDoc::IO, relationship::SpdxRelationshipV2)
     write(TagValueDoc, relationship.SPDXID * "  " * relationship.RelationshipType * "  " * relationship.RelatedSPDXID * "\n")
     ismissing(relationship.Comment) || write_TagValue!(TagValueDoc, relationship.Comment, SpdxRelationshipV2_NameTable.TagValueName[4], SpdxRelationshipV2_NameTable.Multiline[4])
 end
