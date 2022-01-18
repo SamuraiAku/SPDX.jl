d7bb7322019a812e843db407993708aeea9b06bb
Simon Avery <savery@ieee.org>
2022-01-17 23:29:02 -0800

    Minor Code rearrangement

--- src/formatTagValue.jl
+++ src/formatTagValue.jl
@@ -4,19 +4,6 @@ convert_to_TagValue!(TagValueDoc::IO, dataElement::AbstractSpdx)= write(TagValue
 convert_to_TagValue!(TagValueDoc::IO, stringElement::AbstractString)= write(TagValueDoc, stringElement * "\n")
 convert_to_TagValue!(TagValueDoc::IO, boolElement::Bool)= write(TagValueDoc, string(boolElement) * "\n")
 
-function write_TagValue!(TagValueDoc::IO, element, TagValueName::Union{Nothing, String}, Multiline::Bool)
-    if TagValueName !== nothing
-        write(TagValueDoc, string(TagValueName) * ":  ")
-        if Multiline == true
-            write(TagValueDoc, "<text>")
-        end
-    end
-    convert_to_TagValue!(TagValueDoc, element)
-    if (TagValueName !== nothing) && (Multiline == true)
-        write(TagValueDoc, "    </text>\n")
-    end
-end
-
 function convert_to_TagValue!(TagValueDoc::IO, doc::AbstractSpdxData, NameTable::Table)
     for idx in range(1,length= length(NameTable))
         fieldval= getproperty(doc, NameTable.Symbol[idx])
@@ -48,4 +35,18 @@ end
 function convert_to_TagValue!(TagValueDoc::IO, relationship::SpdxRelationshipV2)
     write(TagValueDoc, relationship.SPDXID * "  " * relationship.RelationshipType * "  " * relationship.RelatedSPDXID * "\n")
     ismissing(relationship.Comment) || write_TagValue!(TagValueDoc, relationship.Comment, SpdxRelationshipV2_NameTable.TagValueName[4], SpdxRelationshipV2_NameTable.Multiline[4])
+end
+
+#########################
+function write_TagValue!(TagValueDoc::IO, element, TagValueName::Union{Nothing, String}, Multiline::Bool)
+    if TagValueName !== nothing
+        write(TagValueDoc, string(TagValueName) * ":  ")
+        if Multiline == true
+            write(TagValueDoc, "<text>")
+        end
+    end
+    convert_to_TagValue!(TagValueDoc, element)
+    if (TagValueName !== nothing) && (Multiline == true)
+        write(TagValueDoc, "    </text>\n")
+    end
 end
