f972264f84724e9feb227e49153065f309fc1de8
Simon Avery <savery@ieee.org>
2022-02-11 08:38:02 -0800

    Rearrange precompile code. Precompile most JSON writing code.

--- src/SPDX.jl
+++ src/SPDX.jl
@@ -31,68 +31,107 @@ include("api.jl")
 # Write your package code here.
 
 function __init__()
-    # Force compilation of all SPDX data types
-    a= SpdxRelationshipV2("DOCA CONTAINS FILEB")
-    b= SpdxSimpleLicenseExpressionV2("MIT")
-    c= SpdxCreatorV2("Person: Me ()")
-    d= SpdxTimeV2(now())
-    e= SpdxChecksumV2("SHA1: 85ed0817af83a24ad8da68c2b5094de69833983c")
-    f= SpdxAnnotationV2()
-    g= SpdxLicenseInfoV2("LicID")
-    h= SpdxLicenseCrossReferenceV2()
-    i= SpdxPackageExternalReferenceV2("SECURITY cpe23Type cpe:2.3:a:pivotal_software:spring_framework:4.1.0:*:*:*:*:*:*:*")
-    j= SpdxPkgVerificationCodeV2("d6a770ba38583ed4bb4525bd96e50461655d2758 (excludes: ./package.spdx)")
-    k= SpdxPackageV2("MyID")
-    l= SpdxCreationInfoV2()
-    m= SpdxNamespaceV2("http://spdx.org/spdxdocs/spdx-example-444504E0-4F89-41D3-9A0C-0305E82C3301")
-    n= SpdxDocumentExternalReferenceV2("DocumentRef-spdx-tool-1.2 http://spdx.org/spdxdocs/spdx-tools-v1.2-3F2504E0-4F89-41D3-9A0C-0305E82C3301 SHA1: d6a770ba38583ed4bb4525bd96e50461655d2759")
-    z= SpdxDocumentV2()
+    # Force compilation of all SPDX data types + string()
 
-    # Precompile custom accessor methods
-    precompile(getproperty,   (SpdxDocumentV2, Symbol))
-    precompile(setproperty!,   (SpdxDocumentV2, Symbol, Any))
-    precompile(propertynames, (SpdxDocumentV2,))
-
-    precompile(getproperty,   (SpdxAnnotationV2, Symbol))
-    precompile(setproperty!,  (SpdxAnnotationV2, Symbol, Any))
-    precompile(propertynames, (SpdxAnnotationV2,))
-
-    precompile(getproperty,   (SpdxCreationInfoV2, Symbol))
-    precompile(setproperty!,  (SpdxCreationInfoV2, Symbol, Any))
-    precompile(propertynames, (SpdxCreationInfoV2,))
-
-    precompile(getproperty,   (SpdxLicenseCrossReferenceV2, Symbol))
-    precompile(setproperty!,  (SpdxLicenseCrossReferenceV2, Symbol, Any))
-    precompile(propertynames, (SpdxLicenseCrossReferenceV2,))
-
-    precompile(getproperty,   (SpdxPackageExternalReferenceV2, Symbol))
-    precompile(setproperty!,  (SpdxPackageExternalReferenceV2, Symbol, Any))
-    precompile(propertynames, (SpdxPackageExternalReferenceV2,))
-
-    precompile(getproperty,   (SpdxPackageV2, Symbol))
-    precompile(setproperty!,  (SpdxPackageV2, Symbol, Any))
-    precompile(propertynames, (SpdxPackageV2,))
-
+    #### SpdxRelationshipV2
+    a= string(SpdxRelationshipV2("DOCA CONTAINS FILEB"))
     precompile(getproperty,   (SpdxRelationshipV2, Symbol))
     precompile(setproperty!,  (SpdxRelationshipV2, Symbol, Any))
     precompile(propertynames, (SpdxRelationshipV2,))
+    precompile(convert_to_JSON, (SpdxRelationshipV2, typeof(SpdxRelationshipV2_NameTable)))
+    #precompile(convert_from_JSON, (Dict{String, Any}, typeof(SpdxRelationshipV2_NameTable), Function))
 
-    # Precompile string methods for all SPDX types
-    precompile(string, (SpdxSimpleLicenseExpressionV2,))
-    precompile(string, (SpdxCreatorV2,))
-    precompile(string, (SpdxTimeV2,))
-    precompile(string, (SpdxDocumentV2,))
-    precompile(string, (SpdxChecksumV2,))
-    precompile(string, (SpdxAnnotationV2,))
-    precompile(string, (SpdxLicenseInfoV2,))
-    precompile(string, (SpdxLicenseCrossReferenceV2,))
-    precompile(string, (SpdxRelationshipV2,))
-    precompile(string, (SpdxPackageExternalReferenceV2,))
-    precompile(string, (SpdxPkgVerificationCodeV2,))
-    precompile(string, (SpdxPackageV2,))
-    precompile(string, (SpdxDocumentExternalReferenceV2,))
-    precompile(string, (SpdxNamespaceV2,))
-    precompile(string, (SpdxCreationInfoV2,))
+    #### SpdxSimpleLicenseExpressionV2
+    b= string(SpdxSimpleLicenseExpressionV2("MIT"))
+
+    #### SpdxCreatorV2
+    c= string(SpdxCreatorV2("Person: Me ()"))
+
+    #### SpdxTimeV2
+    d= string(SpdxTimeV2(now()))
+
+    #### SpdxChecksumV2
+    e= string(SpdxChecksumV2("SHA1: 85ed0817af83a24ad8da68c2b5094de69833983c"))
+    precompile(convert_to_JSON, (SpdxChecksumV2, typeof(SpdxChecksumV2_NameTable)))
+    #precompile(convert_from_JSON, (Dict{String, Any}, typeof(SpdxChecksumV2_NameTable), Function))
+
+    #### SpdxAnnotationV2
+    f= string(SpdxAnnotationV2())
+    precompile(getproperty,   (SpdxAnnotationV2, Symbol))
+    precompile(setproperty!,  (SpdxAnnotationV2, Symbol, Any))
+    precompile(propertynames, (SpdxAnnotationV2,))
+    precompile(convert_to_JSON, (SpdxAnnotationV2, typeof(SpdxAnnotationV2_NameTable)))
+    #precompile(convert_from_JSON, (Dict{String, Any}, typeof(SpdxAnnotationV2_NameTable), Function))
+
+    #### SpdxLicenseInfoV2
+    g= string(SpdxLicenseInfoV2("LicID"))
+    precompile(getproperty,   (SpdxLicenseInfoV2, Symbol))
+    precompile(setproperty!,  (SpdxLicenseInfoV2, Symbol, Any))
+    precompile(propertynames, (SpdxLicenseInfoV2,))
+    precompile(convert_to_JSON, (SpdxLicenseInfoV2, typeof(SpdxLicenseInfoV2_NameTable)))
+    #precompile(convert_from_JSON, (Dict{String, Any}, typeof(SpdxLicenseInfoV2_NameTable), Function))
+
+
+    #### SpdxLicenseCrossReferenceV2
+    h= string(SpdxLicenseCrossReferenceV2())
+    precompile(getproperty,   (SpdxLicenseCrossReferenceV2, Symbol))
+    precompile(setproperty!,  (SpdxLicenseCrossReferenceV2, Symbol, Any))
+    precompile(propertynames, (SpdxLicenseCrossReferenceV2,))
+    precompile(convert_to_JSON, (SpdxLicenseCrossReferenceV2, typeof(SpdxLicenseCrossReferenceV2_NameTable)))
+    #precompile(convert_from_JSON, (Dict{String, Any}, typeof(SpdxLicenseCrossReferenceV2_NameTable), Function))
+    
+    #### SpdxPackageExternalReferenceV2
+    i= string(SpdxPackageExternalReferenceV2("SECURITY cpe23Type cpe:2.3:a:pivotal_software:spring_framework:4.1.0:*:*:*:*:*:*:*"))
+    precompile(getproperty,   (SpdxPackageExternalReferenceV2, Symbol))
+    precompile(setproperty!,  (SpdxPackageExternalReferenceV2, Symbol, Any))
+    precompile(propertynames, (SpdxPackageExternalReferenceV2,))
+    precompile(convert_to_JSON, (SpdxPackageExternalReferenceV2, typeof(SpdxPackageExternalReferenceV2_NameTable)))
+    #precompile(convert_from_JSON, (Dict{String, Any}, typeof(SpdxPackageExternalReferenceV2_NameTable), Function))
+
+    
+    #### SpdxPkgVerificationCodeV2
+    j= string(SpdxPkgVerificationCodeV2("d6a770ba38583ed4bb4525bd96e50461655d2758 (excludes: ./package.spdx)"))
+    precompile(convert_to_JSON, (SpdxPkgVerificationCodeV2, typeof(SpdxPkgVerificationCodeV2_NameTable)))
+    #precompile(convert_from_JSON, (Dict{String, Any}, typeof(SpdxPkgVerificationCodeV2_NameTable), Function))
+
+
+    #### SpdxPackageV2
+    k= string(SpdxPackageV2("MyID"))
+    precompile(getproperty,   (SpdxPackageV2, Symbol))
+    precompile(setproperty!,  (SpdxPackageV2, Symbol, Any))
+    precompile(propertynames, (SpdxPackageV2,))
+    precompile(convert_to_JSON, (SpdxPackageV2, typeof(SpdxPackageV2_NameTable)))
+    #precompile(convert_from_JSON, (Dict{String, Any}, typeof(SpdxPackageV2_NameTable), Function))
+
+
+    #### SpdxCreationInfoV2
+    l= string(SpdxCreationInfoV2())
+    precompile(getproperty,   (SpdxCreationInfoV2, Symbol))
+    precompile(setproperty!,  (SpdxCreationInfoV2, Symbol, Any))
+    precompile(propertynames, (SpdxCreationInfoV2,))
+    precompile(convert_to_JSON, (SpdxCreationInfoV2, typeof(SpdxCreationInfoV2_NameTable)))
+    #precompile(convert_from_JSON, (Dict{String, Any}, typeof(SpdxCreationInfoV2_NameTable), Function))
+
+    #### SpdxNamespaceV2
+    m= string(SpdxNamespaceV2("http://spdx.org/spdxdocs/spdx-example-444504E0-4F89-41D3-9A0C-0305E82C3301"))
+    
+    #### SpdxDocumentExternalReferenceV2
+    n= string(SpdxDocumentExternalReferenceV2("DocumentRef-spdx-tool-1.2 http://spdx.org/spdxdocs/spdx-tools-v1.2-3F2504E0-4F89-41D3-9A0C-0305E82C3301 SHA1: d6a770ba38583ed4bb4525bd96e50461655d2759"))
+    precompile(convert_to_JSON, (SpdxDocumentExternalReferenceV2, typeof(SpdxDocumentExternalReferenceV2_NameTable)))
+    #precompile(convert_from_JSON, (Dict{String, Any}, typeof(SpdxDocumentExternalReferenceV2_NameTable), Function))
+
+    #### SpdxDocumentV2
+    z= string(SpdxDocumentV2())
+    precompile(getproperty,   (SpdxDocumentV2, Symbol))
+    precompile(setproperty!,  (SpdxDocumentV2, Symbol, Any))
+    precompile(propertynames, (SpdxDocumentV2,))
+    precompile(convert_to_JSON, (SpdxDocumentV2, typeof(SpdxDocumentV2_NameTable)))
+    precompile(compute_additional_JSON_fields!, (OrderedDict{String, Any}, SpdxDocumentV2))
+    #precompile(convert_from_JSON, (Dict{String, Any}, typeof(SpdxDocumentV2_NameTable), Function))
+
+    
+    
+    
 end
 
 
