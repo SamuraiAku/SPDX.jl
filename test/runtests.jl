0ff5b71242601df08cd22a53682916fab9c667c4
Simon Avery <savery@ieee.org>
2022-04-15 12:31:05 -0700

    Add tests for SpdxRelationshipV2

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
+# SPDX-License-Identifier: MIT
+
 using SPDX
 using Test
 
-@testset "SPDX.jl" begin
-    # Write your tests here.
+@testset "Relationship" begin
+    a= SpdxRelationshipV2("SPDX-Ref1", "CONTAINS", "SPDX-Ref2") # Create object and populate directly
+    b= SpdxRelationshipV2(" SPDX-Ref1     CONTAINS  SPDX-Ref2 ")  # Create object via string parsing. Add extra spaces to make it interesting
+    @test SPDX.compare_b(a, b)
+
+    # Create object via Dictionary parse (from JSON read)
+    c_dict= Dict{String, Any}("spdxElementId" => "SPDX-Ref1", "relationshipType" => "CONTAINS", "relatedSpdxElement" => "SPDX-Ref2")
+    c= SPDX.convert_from_JSON(c_dict, SPDX.SpdxRelationshipV2_NameTable, SpdxRelationshipV2)
+    @test SPDX.compare_b(a, c)
+    
+    # Create object via TagValue parse
+    d_tv= IOBuffer("Relationship: SPDX-Ref1 CONTAINS SPDX-Ref2\nRelationshipComment: <text> This is a comment.\n In two lines. </text>")
+    d= SPDX.parse_TagValue(d_tv, SPDX.SpdxRelationshipV2_NameTable, SPDX.SpdxRelationshipV2)
+    @test !SPDX.compare_b(a, d)
+    @test SPDX.compare_b(a, d; skipproperties= Symbol[:Comment])
 end
