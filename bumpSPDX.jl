700ebd82af1292ec7defdfe61b7890eb8911f585
Simon Avery <savery@ieee.org>
2023-05-14 08:34:59 -0700

    Update bumpSPDX.jl to use PkgToSBOM

--- bumpSPDX.jl
+++ bumpSPDX.jl
@@ -1,30 +1,33 @@
 # SPDX-License-Identifier: MIT
 
+using PkgToSBOM
 using SPDX
 using Pkg
+using UUIDs
 
-spdxDoc= SPDX.readspdx("SPDX_jl.spdx.json")
-spdxPkgNames= [pkg.Name for pkg in spdxDoc.Packages]
+spdxFileName= "SPDX.spdx.json"
+myName= SpdxCreatorV2("Person", "Simon Avery", "savery@ieee.org")
+myTool= SpdxCreatorV2("Tool", "PkgToSBOM.jl", "")
+myLicense= SpdxLicenseExpressionV2("MIT")
 
-envpkgs= Pkg.dependencies()
-SpdxPackageInfo= envpkgs[Pkg.project().dependencies["SPDX"]]
+myPackage_instr= spdxPackageInstructions(
+              spdxfile_toexclude= [spdxFileName],
+              originator= myName,
+              declaredLicense= myLicense,
+              copyright= "Copyright (c) 2022 Simon Avery <savery@ieee.org> and contributors",
+              name= "SPDX")
 
-pkgidx= findfirst("SPDX.jl" .== spdxPkgNames)
-spdxDoc.Packages[pkgidx].Version= string(SpdxPackageInfo.version)
+devRoot= filter(p-> p.first == "SPDX", Pkg.project().dependencies)
+myNamespace= "https://github.com/SamuraiAku/SPDX.jl/blob/main/SPDX.spdx.json"
 
-for (spdxdep, uuid) in SpdxPackageInfo.dependencies
-    version= envpkgs[uuid].version
-    isnothing(version) && continue # Go to next iteration if it's a standard library
-    local pkgidx= findfirst(spdxdep*".jl" .== spdxPkgNames)
-    if isnothing(pkgidx)
-        println("WARNING: SPDX dependency ", spdxdep, " is not in the SPDX Document. Please add the package and rerun this script")
-        continue
-    end
-    spdxDoc.Packages[pkgidx].Version= string(version)
-    spdxDoc.Packages[pkgidx].DownloadLocation= SpdxDownloadLocationV2(spdxDoc.Packages[pkgidx].DownloadLocation; VCS_Tag= "v"*string(version))
-end
+active_pkgs= Pkg.project().dependencies;
+SPDX_docCreation= spdxCreationData(
+              Name= "SPDX.jl Developer SBOM",
+              Creators= [myName, myTool],
+              NamespaceURL= myNamespace,
+              rootpackages= devRoot,
+              packageInstructions= Dict{UUID, spdxPackageInstructions}(active_pkgs[myPackage_instr.name] => myPackage_instr)
+            )
 
-updatenamespace!(spdxDoc)
-setcreationtime!(spdxDoc)
-
-SPDX.writespdx(spdxDoc, "SPDX_jl.spdx.json")
+sbom= generateSPDX(SPDX_docCreation)
+writespdx(sbom, spdxFileName)
