4e4214a09d797dac7f2e12a833a54532eb34462f
Simon Avery <savery@ieee.org>
2023-07-10 23:11:00 -0700

    Convert SpdxFileV2 to a mutable struct with keywords

--- src/spdxFile.jl
+++ src/spdxFile.jl
@@ -19,7 +19,6 @@ end
 #############################################
 const SpdxFileV2_NameTable= Table(
          Symbol= [ :Name,       :SPDXID,    :Type,                       :Checksum,                    :LicenseConcluded,               :LicensesInFile,                                                                  :LicenseComments,    :Copyright,             :FileComments,   :Notice,         :Contributors,         :Attributions,           :Annotations      ],
-        Default= [  nothing,     nothing,    Vector{SpdxFileTypeV2}(),    Vector{SpdxChecksumV2}(),     missing,                         Vector{Union{SpdxSimpleLicenseExpressionV2, SpdxComplexLicenseExpressionV2}}(),   missing,             missing,                missing,         missing,         Vector{String}(),      Vector{String}(),        Vector{SpdxAnnotationV2}(),  ],
         Mutable= [  false,       false,      true,                        true,                         true,                            true,                                                                             true,                true,                   true,            true,            true,                  true,                    true,                        ],
     Constructor= [  string,      string,     SpdxFileTypeV2,              SpdxChecksumV2,               SpdxLicenseExpressionV2,         SpdxLicenseExpressionV2,                                                          string,              string,                 string,          string,          string,                string,                  SpdxAnnotationV2,            ],
       NameTable= [  nothing,     nothing,    nothing,                     SpdxChecksumV2_NameTable,     nothing,                         nothing,                                                                          nothing,             nothing,                nothing,         nothing,         nothing,               nothing,                 SpdxAnnotationV2_NameTable,  ],
@@ -28,16 +27,22 @@ const SpdxFileV2_NameTable= Table(
    TagValueName= [  "FileName",  "SPDXID",   "FileType",                  "FileChecksum",               "LicenseConcluded",              "LicenseInfoInFile",                                                              "LicenseComments",   "FileCopyrightText",    "FileComment",   "FileNotice",    "FileContributor",     "FileAttributionText",   "Annotator",                 ],
 )
 
-struct SpdxFileV2 <: AbstractSpdxData
-    Name::String
-    SPDXID::String
-    MutableFields::OrderedDict{Symbol, Any}
+Base.@kwdef mutable struct SpdxFileV2 <: AbstractSpdxData2
+    const Name::String
+    const SPDXID::String
+    Type::Vector{SpdxFileTypeV2}= Vector{SpdxFileTypeV2}()
+    Checksum::Vector{SpdxChecksumV2}= Vector{SpdxChecksumV2}()
+    LicenseConcluded::Union{Missing, SpdxComplexLicenseExpressionV2, SpdxSimpleLicenseExpressionV2}= missing
+    LicensesInFile::Vector{Union{SpdxSimpleLicenseExpressionV2, SpdxComplexLicenseExpressionV2}}= Vector{Union{SpdxSimpleLicenseExpressionV2, SpdxComplexLicenseExpressionV2}}()
+    LicenseComments::Union{Missing, String}= missing
+    Copyright::Union{Missing, String}= missing
+    FileComments::Union{Missing, String}= missing
+    Notice::Union{Missing, String}= missing
+    Contributors::Vector{String}= String[]
+    Attributions::Vector{String}= String[]
+    Annotations::Vector{SpdxAnnotationV2}= SpdxAnnotationV2[]
 end
 
 function SpdxFileV2(Name::AbstractString, SPDXID::AbstractString)
-    MutableFields= init_MutableFields(SpdxFileV2_NameTable)
-    return SpdxFileV2(Name, SPDXID, MutableFields)
+    return SpdxFileV2(Name= Name, SPDXID= SPDXID)
 end
-
-
-
