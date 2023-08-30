702b5b0d366c001edd5ca1afd605e13a39aeb6f5
hannahilea <hannahilea@users.noreply.github.com>
2023-08-30 15:46:01 -0400

    rename

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,6 @@ using JSON
 using Dates
 using TimeZones
 
-include("test_api.jl")
 include("test_types.jl")
 include("test_SpdxAnnotation.jl")
 include("test_SpdxLicense.jl")
@@ -15,3 +14,4 @@ include("test_SpdxSnippet.jl")
 include("test_SpdxFile.jl")
 include("test_SpdxPackage.jl")
 include("test_SpdxDocument.jl")
+include("test_read_write.jl")
