df1dd4f57d4b9774454c980572dad6a18020b2e2
hannahilea <hannahilea@users.noreply.github.com>
2023-09-12 14:08:54 -0400

    add more tests

--- test/test_api.jl
+++ test/test_api.jl
@@ -1,7 +1,17 @@
-@testset "`isequal`" begin
-    # TODO
-end
+@testset "Base extensions" begin
+    sbom_path = joinpath(pkgdir(SPDX), "SPDX.spdx.json")
+    mysbom= readspdx(sbom_path)
+    mysbom2= readspdx(sbom_path)
 
-@testset "`==`" begin
-    # TODO
+    @testset "`Base.==`" begin
+        # mysbom contains `missing` entries...
+        @test ismissing(mysbom.CreationInfo.LicenseListVersion)
+        # ...so a test of `==` should also be missing:
+        @test ismissing(mysbom == mysbom)
+    end
+
+    @testset "`Base.isequal`" begin
+        @test isequal(mysbom, mysbom)
+        @test isequal(mysbom, mysbom2)
+    end
 end
